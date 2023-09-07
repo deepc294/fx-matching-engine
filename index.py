c979c8999800af9eb463b927e393b2734954f6a2
Deepesh Choudhary <deepc294@gmail.com>
2023-09-08 00:02:59 +0530

    feat: Created Flask app with basic order format

---
+++ index.py
@@ -0,0 +1,16 @@
+'''
+    user_id: string
+    order_id: number
+    from_curr: string
+    to_curr: string
+    min_bp: number
+    max_bp: number
+    recommended_bp: number
+    quantity: number
+    timestamp: number
+    status: string
+    order_type: string
+    fill_type: string
+    remaining_quantity: number
+    fill: dict_array
+'''
